1188005616c6847f959303441c597065d63d4998
SomneelMaity <somneel.maity@gmail.com>
2026-06-14 08:08:48 +0530

    PDF RAG Contd...

--- Projects/IIT_KGP/PDF/backend/main.py
+++ Projects/IIT_KGP/PDF/backend/main.py
@@ -14,6 +14,9 @@ app = FastAPI()
 #model call
 model = SentenceTransformer('all-MiniLM-L6-v2')  # Load the pre-trained model for generating embeddings
 
+# In Memory Storage for the embeddings and metadata
+documents = {}
+
 # cleaning the text by removing extra spaces and newlines, and stripping leading/trailing whitespace
 def clean_text(text):
     text = re.sub(r'\s+', ' ', text)  # Replace multiple whitespace with a single space
@@ -48,6 +51,11 @@ async def upload_pdf(file: UploadFile = File(...)):
     page_chunk = chunk(cleaned_text)
     embeddings = model.encode(page_chunk)  # Generate embeddings for each chunk of text
 
+    documents[file.filename] = {    # Store the metadata and embeddings in the in-memory storage
+        "chunks": page_chunk,
+        "embeddings": embeddings
+    }
+
     return {
         "filename": file.filename,
         "saved": path,
@@ -55,3 +63,28 @@ async def upload_pdf(file: UploadFile = File(...)):
         "preview": cleaned_text[:100],  # Return the first 100 characters as a preview
         "first_chunk": page_chunk[0]
     }
+
+@app.get("/documents")      # Endpoint to retrieve the list of uploaded documents along with their metadata (filename and embeddings shape)
+def get_document():
+    return {
+        "uploaded_documents": list(documents.keys())
+    }
+
+@app.get("/documents/{filename}")   # Endpoint to retrieve the metadata and embeddings for a specific document based on its filename
+def get_document_by_filename(filename: str):
+    if filename not in documents:
+        return {"error": "File not found"}
+    return {
+        "filename": filename,
+        "embeddings": list(documents[filename]["embeddings"].shape),  # Convert numpy array to list for JSON serialization
+    }
+
+
+# Upload pdf
+# save pdf
+# extract text from pdf
+# clean text
+# chunking of text
+# creating embeddings for each chunk
+# store it in memory / db
+# chatting system
